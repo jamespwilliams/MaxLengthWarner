5895d60c19217f0788921344966502caef6f01aa
James Williams <jamesphwilliams@gmail.com>
2019-09-07 17:27:42 +0100

    Add eventListener for input on existing input tags w/ maxlength attr

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,20 @@
+function setUpListener() {
+
+    document.querySelectorAll("input[maxlength]").forEach(input => {
+        input.addEventListener("keydown", function(e) {
+            const maxLen = e.target.getAttribute("maxlength");
+
+            if (e.target.value.length == maxLen) {
+                 
+            }
+        });
+    });
+
+    document.arrive(".test-elem", function() {
+        // 'this' refers to the newly created element
+    });
+}
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
@@ -8,6 +25,7 @@ chrome.extension.sendMessage({}, function(response) {
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
 
+        setUpListener();
 	}
 	}, 10);
 });
